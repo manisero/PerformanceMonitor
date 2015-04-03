d13b42cb36a3a2eec91ed2dad7bc7efe0e52d8f1
Michal Aniserowicz <m.aniserowicz@funandmobile.com>
2015-04-03 19:35:24 +0200

    Started implementing report formatting

---
+++ src/Manisero.PerformanceMonitor/Manisero.PerformanceMonitor.Tests/ReportFormattingTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using Xunit;
+
+namespace Manisero.PerformanceMonitor.Tests
+{
+	public class ReportFormattingTests
+	{
+		[Fact]
+		public void formats_default_report()
+		{
+			// Arrange
+			var monitor = new PerformanceMonitor<string>();
+			var task1 = "task1";
+			var task2 = "task2";
+
+			// Act
+			monitor.StartTask(task1);
+			monitor.StartTask(task2);
+
+			// Assert
+			var report = monitor.GetReport();
+		}
+	}
+}
--- src/Manisero.PerformanceMonitor/Manisero.PerformanceMonitor/PerformanceMonitor.cs
+++ src/Manisero.PerformanceMonitor/Manisero.PerformanceMonitor/PerformanceMonitor.cs
@@ -31,5 +31,31 @@ namespace Manisero.PerformanceMonitor
 		{
 			return _tasks.ToDictionary(x => x.Key, x => x.Value.Elapsed);
 		}
+
+		public string GetReport(bool includeHeader = true, Func<TTask, string> taskNameFormatter = null, Func<TimeSpan, string> taskDurationFormatter = null)
+		{
+			if (taskNameFormatter == null)
+			{
+				taskNameFormatter = x => x.ToString();
+			}
+
+			if (taskDurationFormatter == null)
+			{
+				taskDurationFormatter = x => x.TotalMilliseconds.ToString();
+			}
+
+			var report = GetResults().Select(x => string.Format("{0}: {1}",
+																taskNameFormatter(x.Key),
+																taskDurationFormatter(x.Value)))
+									 .ToList();
+
+			if (includeHeader)
+			{
+				var header = typeof(TTask).Name + " performance report:";
+				report.Insert(0, header);
+			}
+
+			return string.Join(Environment.NewLine, report);
+		}
     }
 }
