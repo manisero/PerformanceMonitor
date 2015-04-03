91f53d81e3aacbeeb5bcab3a2e1cb5fd04b09821
Michal Aniserowicz <m.aniserowicz@funandmobile.com>
2015-04-03 20:02:03 +0200

    Tested report formatting

--- src/Manisero.PerformanceMonitor/Manisero.PerformanceMonitor.Tests/ReportFormattingTests.cs
+++ src/Manisero.PerformanceMonitor/Manisero.PerformanceMonitor.Tests/ReportFormattingTests.cs
@@ -1,11 +1,12 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
 
 namespace Manisero.PerformanceMonitor.Tests
 {
 	public class ReportFormattingTests
 	{
-		[Test]
-		public void formats_default_report()
+		private PerformanceMonitor<string> Execute()
 		{
 			// Arrange
 			var monitor = new PerformanceMonitor<string>();
@@ -15,9 +16,51 @@ namespace Manisero.PerformanceMonitor.Tests
 			// Act
 			monitor.StartTask(task1);
 			monitor.StartTask(task2);
+			Thread.Sleep(20);
+			monitor.StopTask(task1);
+			monitor.StopTask(task2);
+
+			return monitor;
+		}
+
+		[Test]
+		public void formats_default_report()
+		{
+			var monitor = Execute();
 
 			// Assert
 			var report = monitor.GetReport();
+			Console.Write(report);
+		}
+
+		[Test]
+		public void formats_report_without_header()
+		{
+			var monitor = Execute();
+
+			// Assert
+			var report = monitor.GetReport(false);
+			Console.Write(report);
+		}
+
+		[Test]
+		public void formats_report_with_custom_task_name_formatter()
+		{
+			var monitor = Execute();
+
+			// Assert
+			var report = monitor.GetReport(taskNameFormatter: x => x.ToUpper());
+			Console.Write(report);
+		}
+
+		[Test]
+		public void formats_report_with_custom_task_duration_formatter()
+		{
+			var monitor = Execute();
+
+			// Assert
+			var report = monitor.GetReport(taskDurationFormatter: x => x.ToString());
+			Console.Write(report);
 		}
 	}
 }
