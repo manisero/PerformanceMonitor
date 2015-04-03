e813bec40bfe0a9068595857da5e59b7e04727f9
Michal Aniserowicz <m.aniserowicz@funandmobile.com>
2015-04-03 17:59:33 +0200

    Created tests project

---
+++ src/Manisero.PerformanceMonitor/Manisero.PerformanceMonitor.Tests/PerformanceMonitorTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Manisero.PerformanceMonitor.Tests
+{
+    public class PerformanceMonitorTests
+    {
+		[Fact]
+		public void Passing()
+		{
+			Assert.Equal(3, 3);
+		}
+
+		[Fact]
+		public void Failing()
+		{
+			Assert.Equal(3, 5);
+		}
+    }
+}
