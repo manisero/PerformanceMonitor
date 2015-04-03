41963ba1709027cbfc6db3f94f4cf0c81c4064cf
Michal Aniserowicz <m.aniserowicz@funandmobile.com>
2015-04-03 23:35:03 +0200

    Implemented formatting nested report

---
+++ src/Manisero.PerformanceMonitor/Manisero.PerformanceMonitor.Tests/NestingPerformanceMonitor/ReportFormattingTests.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Threading;
+using Manisero.PerformanceMonitor._Impl;
+using NUnit.Framework;
+
+namespace Manisero.PerformanceMonitor.Tests.NestingPerformanceMonitor
+{
+	public class ReportFormattingTests
+	{
+		private NestingPerformanceMonitor<string> Execute()
+		{
+			// Arrange
+			var monitor = new NestingPerformanceMonitor<string>();
+			var task1 = "task1";
+			var subtask1_1 = "sub-task 1.1";
+			var subsubtask1_1_1 = "sub-sub-task 1.1.1";
+			var subsubtask1_1_2 = "sub-sub-task 1.1.2";
+			var subtask1_2 = "sub-task 1.2";
+			var subsubtask1_2_1 = "sub-sub-task 1.2.1";
+			var subsubtask1_2_2 = "sub-sub-task 1.2.2";
+
+			var task2 = "task2";
+			var subtask2_1 = "sub-task 2.1";
+			var subsubtask2_1_1 = "sub-sub-task 2.1.1";
+			var subsubtask2_1_2 = "sub-sub-task 2.1.2";
+			var subtask2_2 = "sub-task 2.2";
+			var subsubtask2_2_1 = "sub-sub-task 2.2.1";
+			var subsubtask2_2_2 = "sub-sub-task 2.2.2";
+
+			// Act - task1:
+			monitor.StartTask(task1);
+			monitor.StartTask(subtask1_1); // subtask1_1
+			monitor.StartTask(subsubtask1_1_1);
+			Thread.Sleep(20);
+			monitor.StopTask(subsubtask1_1_1);
+			monitor.StartTask(subsubtask1_1_2);
+			Thread.Sleep(20);
+			monitor.StopTask(subsubtask1_1_2);
+			monitor.StopTask(subtask1_1);
+			monitor.StartTask(subtask1_2); // subtask1_2
+			monitor.StartTask(subsubtask1_2_1);
+			Thread.Sleep(20);
+			monitor.StopTask(subsubtask1_2_1);
+			monitor.StartTask(subsubtask1_2_2);
+			Thread.Sleep(20);
+			monitor.StopTask(subsubtask1_2_2);
+			monitor.StopTask(subtask1_2);
+			monitor.StopTask(task1);
+
+			// Act - task2:
+			monitor.StartTask(task2);
+			monitor.StartTask(subtask2_1); // subtask2_1
+			monitor.StartTask(subsubtask2_1_1);
+			Thread.Sleep(20);
+			monitor.StopTask(subsubtask2_1_1);
+			monitor.StartTask(subsubtask2_1_2);
+			Thread.Sleep(20);
+			monitor.StopTask(subsubtask2_1_2);
+			monitor.StopTask(subtask2_1);
+			monitor.StartTask(subtask2_2); // subtask2_2
+			monitor.StartTask(subsubtask2_2_1);
+			Thread.Sleep(20);
+			monitor.StopTask(subsubtask2_2_1);
+			monitor.StartTask(subsubtask2_2_2);
+			Thread.Sleep(20);
+			monitor.StopTask(subsubtask2_2_2);
+			monitor.StopTask(subtask2_2);
+			monitor.StopTask(task2);
+
+			return monitor;
+		}
+
+		[Test]
+		public void formats_default_report()
+		{
+			var monitor = Execute();
+
+			// Assert
+			var report = monitor.GetReport();
+			Console.Write(report);
+		}
+
+		[Test]
+		public void formats_report_without_header()
+		{
+			var monitor = Execute();
+
+			// Assert
+			var report = monitor.GetReport(false);
+			Console.Write(report);
+		}
+
+		[Test]
+		public void formats_report_with_custom_task_name_formatter()
+		{
+			var monitor = Execute();
+
+			// Assert
+			var report = monitor.GetReport(taskNameFormatter: x => x.ToUpper());
+			Console.Write(report);
+		}
+
+		[Test]
+		public void formats_report_with_custom_task_duration_formatter()
+		{
+			var monitor = Execute();
+
+			// Assert
+			var report = monitor.GetReport(taskDurationFormatter: x => x.ToString());
+			Console.Write(report);
+		}
+	}
+}
--- src/Manisero.PerformanceMonitor/Manisero.PerformanceMonitor/_Impl/NestingPerformanceMonitor.cs
+++ src/Manisero.PerformanceMonitor/Manisero.PerformanceMonitor/_Impl/NestingPerformanceMonitor.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 
 namespace Manisero.PerformanceMonitor._Impl
 {
@@ -119,7 +120,49 @@ namespace Manisero.PerformanceMonitor._Impl
 
 		public string GetReport(bool includeHeader = true, Func<TTask, string> taskNameFormatter = null, Func<TimeSpan, string> taskDurationFormatter = null)
 		{
-			throw new NotImplementedException();
+			if (taskNameFormatter == null)
+			{
+				taskNameFormatter = x => x.ToString();
+			}
+
+			if (taskDurationFormatter == null)
+			{
+				taskDurationFormatter = x => x.TotalMilliseconds.ToString();
+			}
+
+			var report = GetSubReport(GetResult(), 0, taskNameFormatter, taskDurationFormatter).ToList();
+
+			if (includeHeader)
+			{
+				var header = typeof(TTask).Name + " performance report:";
+				report.Insert(0, header);
+			}
+
+			return string.Join(Environment.NewLine, report);
+		}
+
+		public IEnumerable<string> GetSubReport(TasksDurations<TTask> tasksDurations, int depth, Func<TTask, string> taskNameFormatter, Func<TimeSpan, string> taskDurationFormatter)
+		{
+			var subreport = new List<string>();
+			var indent = string.Join(string.Empty, Enumerable.Repeat("\t", depth));
+
+			foreach (var taskDuration in tasksDurations)
+			{
+				subreport.Add(string.Format("{0}{1}: {2}",
+											indent,
+											taskNameFormatter(taskDuration.Key),
+											taskDurationFormatter(taskDuration.Value.Duration)));
+
+				if (taskDuration.Value.SubtasksDurations != null)
+				{
+					subreport.AddRange(GetSubReport(taskDuration.Value.SubtasksDurations,
+													depth + 1,
+													taskNameFormatter,
+													taskDurationFormatter));
+				}
+			}
+
+			return subreport;
 		}
 	}
 }
