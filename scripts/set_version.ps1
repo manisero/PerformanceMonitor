016b9111b5a102baada057870b1ef55114dbf18f
Michal Aniserowicz <m.aniserowicz@funandmobile.com>
2015-04-04 14:45:21 +0200

    Implemented set_version script

--- scripts/set_version.ps1
+++ scripts/set_version.ps1
@@ -3,10 +3,12 @@ param(
 )
 
 "Setting version to $version"
-"TODO: Implement"
 
 $assemblyInfoPath = "..\src\Manisero.PerformanceMonitor\Manisero.PerformanceMonitor\Properties\AssemblyInfo.cs"
 $content = Get-Content $assemblyInfoPath
 
-"test" > $assemblyInfoPath
+$content = $content -replace "AssemblyVersion\(""(.*)""\)", "AssemblyVersion(""$version.*"")"
+$content = $content -replace "AssemblyFileVersion\(""(.*)""\)", "AssemblyFileVersion(""$version.*"")"
+$content = $content -replace "AssemblyInformationalVersionAttribute\(""(.*)""\)", "AssemblyInformationalVersionAttribute(""$version"")"
+
 $content > $assemblyInfoPath
