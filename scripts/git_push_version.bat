efeb3224362e9d26f4f9082aee631f004cd485fa
Michal Aniserowicz <m.aniserowicz@funandmobile.com>
2015-04-04 13:43:12 +0200

    Implemented git_push_version script

--- scripts/git_push_version.bat
+++ scripts/git_push_version.bat
@@ -1,2 +1,8 @@
 echo Pushing version %1 to git
-echo TODO: Implement
+
+git add ../src/*/AssemblyInfo.cs
+
+git commit -m "Release version %1"
+git tag -a v%1 -m "Release %1"
+git push
+git push --tags
