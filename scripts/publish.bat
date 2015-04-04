203cc9142110a5e042a96540c27391af4738119d
Michal Aniserowicz <m.aniserowicz@funandmobile.com>
2015-04-04 12:08:37 +0200

    Added rebuild script

--- scripts/publish.bat
+++ scripts/publish.bat
@@ -1,3 +1,7 @@
-call nuget_pack.bat ..\src\Manisero.PerformanceMonitor\Manisero.PerformanceMonitor\Manisero.PerformanceMonitor.csproj
-call nuget_push.bat Manisero.PerformanceMonitor
-del Manisero.PerformanceMonitor.*.nupkg
+SET csprojPath=..\src\Manisero.PerformanceMonitor\Manisero.PerformanceMonitor\Manisero.PerformanceMonitor.csproj
+SET packageName=Manisero.PerformanceMonitor
+
+call rebuild.bat %csprojPath%
+call nuget_pack.bat %csprojPath%
+call nuget_push.bat %packageName%
+del %packageName%.*.nupkg
---
+++ scripts/rebuild.bat
@@ -0,0 +1,2 @@
+echo Rebuilding %1
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe %1 /p:Configuration=Release /t:rebuild
