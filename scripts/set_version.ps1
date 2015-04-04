32719b5996f2bdf31de075ed4f27b7493c16aaf2
Michal Aniserowicz <m.aniserowicz@funandmobile.com>
2015-04-04 15:18:14 +0200

    Fixed set_version script to use proper encoding

--- scripts/set_version.ps1
+++ scripts/set_version.ps1
@@ -5,10 +5,10 @@ param(
 "Setting version to $version"
 
 $assemblyInfoPath = "..\src\Manisero.PerformanceMonitor\Manisero.PerformanceMonitor\Properties\AssemblyInfo.cs"
-$content = Get-Content $assemblyInfoPath
+$content = Get-Content $assemblyInfoPath -Encoding UTF8
 
 $content = $content -replace "AssemblyVersion\(""(.*)""\)", "AssemblyVersion(""$version.*"")"
 $content = $content -replace "AssemblyFileVersion\(""(.*)""\)", "AssemblyFileVersion(""$version.*"")"
 $content = $content -replace "AssemblyInformationalVersionAttribute\(""(.*)""\)", "AssemblyInformationalVersionAttribute(""$version"")"
 
-$content > $assemblyInfoPath
+Set-Content $assemblyInfoPath $content -Encoding UTF8
